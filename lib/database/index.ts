26eb53493eeb6f0783737b6a3ad937f5dcace4f1
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-09-04 23:06:40 +0700

    refactor: test

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,6 +6,7 @@ setTimeout(() => {
     .connect(`mongodb://${process.env.NODE_ENV === 'development' ? 'localhost' : 'mongodb.hop'}:27017/deliverio`)
     // eslint-disable-next-line no-console
     .then(() => console.log('> Database connected'));
+  // help me
 }, 200);
 
 const gameStream = GameModel.watch();
